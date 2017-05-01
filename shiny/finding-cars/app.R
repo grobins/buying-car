effd8e2a98ea5ffe720b8082440de3b9196b4c91
Gareth Robins <gareth.robins@gmail.com>
2017-05-02 07:41:41 +1200

    changed to shiny dash

--- shiny/finding-cars/app.R
+++ shiny/finding-cars/app.R
@@ -14,9 +14,42 @@ require(dplyr)
 require(tidyr)
 require(ggplot2)
 require(knitr)
+require(shinydashboard)
 
 
-getCarData <- function(page, url){
+
+
+
+
+# Define UI for application that draws a histogram
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(
+            textInput('URL', "Enter URL of Search", value = "", width = NULL, placeholder = NULL),
+            textInput('PAGES', "Enter number of search result pages", value = "", width = NULL, placeholder = NULL),
+            sliderInput("param_price_weighting", "Price Weighting:",
+                        min = 0.01, max = 1, value = c(0.6,1)),
+            sliderInput("param_year_weighting", "Year Weighting:",
+                        min = 0.01, max = 1, value = c(0.3,0.7)),
+            sliderInput("param_odo_weighting", "Odomoter Weighting:",
+                        min = 0.01, max = 1, value = c(0.01,0.4)),
+            submitButton("Submit")
+    
+  ),
+  dashboardBody(
+            plotOutput("distPlot"),
+            plotOutput("scatterPlot"),
+            dataTableOutput(outputId="table")
+  )
+)
+
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  getCarData <- function(page, url){
     # Parses the website for the page of the search listings
     # extracts title, price, odometer, year, and location for further analysis
     
@@ -24,6 +57,9 @@ getCarData <- function(page, url){
     theURL   <- gsub('page=.', paste0('page=', page), theURL)
     webpage  <- htmlParse(theURL)
     title    <- xpathSApply(webpage, "//*/div[@class='listingTitle']", xmlValue)
+    titleurl <- xpathSApply(webpage, "//*/div[@class='listingTitle']/a", xmlGetAttr, 'href')
+    titleurl <- paste0("http://www.trademe.co.nz/", titleurl)
+    
     title    <- gsub("[\r\n +\"+]", "", title)
     year     <- sub(".*(\\d+{4}).*$", "\\1", title)
     odometer <- xpathSApply(webpage, "//*/div[@class='rightArrows']", xmlValue)
@@ -32,93 +68,79 @@ getCarData <- function(page, url){
     location <- gsub(",.*$", "", location)
     odometer <- data.frame(odo_data = subset(odometer, grepl("^([1-90-9\\<\\>])",odometer)))
     odometer <- odometer %>% 
-        separate(odo_data, c('odo1', 'odo2', 'vehicle_type', 'engine', 'gearbox'), sep = ',') %>% 
-        mutate(odometer = (as.numeric(odo1) * 1000) + as.numeric(gsub("km", "", odo2))) %>%
-        select(odometer, vehicle_type, engine, gearbox)
+      separate(odo_data, c('odo1', 'odo2', 'vehicle_type', 'engine', 'gearbox'), sep = ',') %>% 
+      mutate(odometer = (as.numeric(odo1) * 1000) + as.numeric(gsub("km", "", odo2))) %>%
+      select(odometer, vehicle_type, engine, gearbox)
     
     price  <- xpathSApply(webpage, "//*/div[@class='listingPrice']", xmlValue)
     price  <- gsub("[\r\n +\"+]", "", price)
     price  <- as.numeric(gsub("[a-zA-Z\\,\\$]", "", price))
-    results <- cbind(title, year, odometer, price, location)
+    results <- cbind(title, year, odometer, price, location, titleurl)
     
     return(results)
     
-}
-
-
-loopThroughPages <- function(pages, url){
+  }
+  
+  
+  loopThroughPages <- function(pages, url){
     # Just a wrapper to look through n pages and get the dataframe result.
     
     for (i in 1:pages){
-        pageResults <- getCarData(i, url)
-        if(i == 1){
-            results <- pageResults
-        } else {
-            results <- rbind(results, pageResults)
-        }
+      pageResults <- getCarData(i, url)
+      if(i == 1){
+        results <- pageResults
+      } else {
+        results <- rbind(results, pageResults)
+      }
     }
     return(results)
-}
-
-getWeightedRanks <- function(df, trial){
+  }
+  
+  getWeightedRanks <- function(df, trial){
     weightings    <- runif(3, 1, 100)  # I started with all metrics having same range
-    weightings[1] <- runif(1, 1, 40)   # but then realised I valued price > odometer > year
-    weightings[2] <- runif(1, 20, 60)
-    weightings[3] <- runif(1, 50, 100)
+    weightings[1] <- runif(1, input$param_price_weighting[1], input$param_price_weighting[2])   # but then realised I valued price > odometer > year
+    weightings[2] <- runif(1, input$param_odo_weighting[1], input$param_odo_weighting[2])
+    weightings[3] <- runif(1, input$param_year_weighting[1], input$param_year_weighting[2])
     rankSet <- df %>% 
-        filter(price <= 15000) %>% 
-        mutate(rankYear = dense_rank(desc(year)),
-               rankOdometer = dense_rank(odometer),
-               rankPrice = dense_rank(price)) %>% 
-        select(title, odometer, rankYear, rankOdometer, rankPrice)
+      filter(price <= 15000) %>% 
+      mutate(rankYear = dense_rank(desc(year)),
+             rankOdometer = dense_rank(odometer),
+             rankPrice = dense_rank(price)) %>% 
+      select(title, odometer, rankYear, rankOdometer, rankPrice)
     df$trialNum <- trial
     df$weightedRank <- rowSums(rankSet[, 2:4] * weightings, na.rm=T) / sum(weightings)
     return(df)
-}
-
-runMonteCarlo <- function(trials, df){
+  }
+  
+  runMonteCarlo <- function(trials, df){
     
     for (i in 1:trials){
-        
-        mc_trial_results <- df %>% filter(price <= 15000) %>% getWeightedRanks(i)
-        
-        if(i == 1){
-            Results <- mc_trial_results
-        } else {
-            Results <- rbind(Results, mc_trial_results)
-        }
+      
+      mc_trial_results <- df %>% filter(price <= 15000) %>% getWeightedRanks(i)
+      
+      if(i == 1){
+        Results <- mc_trial_results
+      } else {
+        Results <- rbind(Results, mc_trial_results)
+      }
     }
     return(Results)
-}
-
-
-
-# Define UI for application that draws a histogram
-ui <- shinyUI(fluidPage(
-    
-    # Application title
-    titlePanel("Analyse Cars on a certain website."),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            textInput('URL', "Enter URL of Search", value = "", width = NULL, placeholder = NULL),
-            textInput('PAGES', "Enter number of search result pages", value = "", width = NULL, placeholder = NULL),
-            submitButton("Submit")
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot"),
-            plotOutput("scatterPlot"),
-            tableOutput(outputId="table")
-        )
-    )
-))
-
-# Define server logic required to draw a histogram
-server <- shinyServer(function(input, output) {
-
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
     carData <- reactive({
         loopThroughPages(input$PAGES, input$URL)
     })
@@ -129,6 +151,7 @@ server <- shinyServer(function(input, output) {
         
     })
     
+
     output$scatterPlot <- renderPlot({
         carData() %>% filter(price <= 15000, odometer<= 130000) %>% 
             ggplot(aes(odometer, price), size = year) + geom_point(aes(size=year)) + geom_smooth() +
@@ -145,16 +168,18 @@ server <- shinyServer(function(input, output) {
     
 
     
-    output$table <- renderTable({
+    output$table <- renderDataTable({
+      print(mcResults()[1,])
         mcResults() %>%
             filter(odometer > 0) %>%
-            group_by(title, odometer, price, year, location) %>%
+            mutate(titleurl = paste0("<a href='",titleurl,"'>","link","</a>")) %>% 
+            group_by(title, odometer, price, year, location, titleurl) %>%
             summarise(medRank = median(weightedRank)) %>%
             arrange(medRank) %>%
             top_n(20)
-    })
+    }, escape = FALSE)
     
-})
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
